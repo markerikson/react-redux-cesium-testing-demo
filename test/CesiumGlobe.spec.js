d7c3c4d8cefa31959faf32eef640685f61447fb5
Mark Erikson <mark@isquaredsoftware.com>
2016-04-17 22:09:07 -0400

    Replace failed fake-ish React/Cesium tests with some real-er ones

--- test/CesiumGlobe.spec.js
+++ test/CesiumGlobe.spec.js
@@ -6,40 +6,50 @@ import {BillboardCollection} from "cesiumDll/Cesium";
 
 import CesiumBillboard from "components/CesiumBillboard";
 
+function createScene() {
+    const add = sinon.spy();
+    const remove = sinon.spy();
+
+    const scene = {
+        primitives : {
+            add,
+            remove
+        },
+
+        isDestroyed() {
+            return false;
+        }
+    };
+
+    return {add, remove, scene};
+}
+
 // Tests of a component that requires Cesium
 // Note that we do NOT try to render a component that actually tries
 // to create a Cesium.Scene, as WebGL is not supported under Node.
 describe("CesiumBillboard", () => {
    it("mounts properly", () => {
         const wrapper = mount(<CesiumBillboard />);
-    })
+    });
 
-    it("Has a BillboardCollection", () => {
-        const wrapper = mount(<CesiumBillboard />);
+    it("Adds its BillboardCollection to a Scene on mount", () => {
+        const {add, scene} = createScene();
 
-        let inst = wrapper.instance();
-        expect(inst.billboards).to.be.an.instanceof(BillboardCollection);
-        expect(inst.billboards.length).to.equal(0);
-    })
+        const wrapper = mount(<CesiumBillboard scene={scene}/>);
 
-    it("Can add a billboard", () => {
-        const wrapper = mount(<CesiumBillboard />);
+        expect(add.calledOnce).to.be.true;
+        const [addaArg] = add.firstCall.args;
+        expect(addaArg).to.be.an.instanceof(BillboardCollection);
+    });
 
-        let inst = wrapper.instance();
-        inst.addBillboard();
+    it("Removes its BillboardCollection on unmount if the Scene is not destroyed", () => {
+        const {remove, scene} = createScene();
 
-        expect(inst.billboards.length).to.equal(1);
-    })
-
-    it("Can unmount", () => {
-        const wrapper = mount(<CesiumBillboard />);
-
-        let inst = wrapper.instance();
+        const wrapper = mount(<CesiumBillboard scene={scene}/>);
 
         wrapper.unmount();
 
-        expect(inst.billboards.length).to.equal(0);
-        expect(inst.billboards.isDestroyed()).to.be.true;
-    })
+        expect(remove.calledOnce).to.be.true;
+    });
 
 });
